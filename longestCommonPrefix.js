24749b2658502246c3768fc772cd3cc07749951b
Corinne Tinacci <corinne.tinacci@gmail.com>
2021-08-26 21:52:26 -0400

    changed <

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -2,7 +2,7 @@ const longestCommonPrefix = (wordsArr) => {
     if (wordsArr.length === 0 || wordsArr.includes('')) return [];
 
     const shortestWord = wordsArr.reduce((accum,word) => {
-        if (word.length > accum.length) accum = word;
+        if (word.length < accum.length) accum = word;
         return accum;
     })
     for (let i = 0; i < wordsArr[0].length; i++){
